e787c913a15c3edadcf9054f068935120ad5dac1
Juan Feito <juanfd17@educastur.es>
2023-09-20 15:29:13 +0200

    ej2 Hoja03_PHP_02

---
+++ ut3/Hoja03_PHP_02/ej2.php
@@ -0,0 +1,9 @@
+<?php
+/* Implementa una función que imprima el mensaje que se le pasa como parámetro y llámala para
+que se ejecute.*/
+    function imprimirMensaje($mensaje){
+        echo $mensaje;
+    }
+
+    imprimirMensaje("holi");
+?>
