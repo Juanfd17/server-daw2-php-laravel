537b185f2176bb0a3ddbfe0e586387661702aa94
Juan Feito <juanfd17@educastur.es>
2023-09-24 11:34:00 +0200

    24/09/2023 ej4 reducir codigo

--- ut3/Hoja03_PHP_02/ej4.php
+++ ut3/Hoja03_PHP_02/ej4.php
@@ -1,6 +1,7 @@
 <?php
 /*Dada una cadena formada por una fecha. Valida si es correcta.*/
-    $fecha = "28/2/2012";
+    $fecha = "31/3/2012";
+    echo (validarFecha($fecha))? "Correcta" : "Incorrecta";
     validarFecha($fecha);
 
     function validarFecha($fecha){
@@ -11,32 +12,18 @@
 
         switch ($mes) {
             case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-                if ($dia > 31) {
-                    echo "Fecha incorrecta";
-                } else {
-                    echo "Fecha correcta";
-                }
+                return !($dia > 31);
                 break;
 
             case 4: case 6: case 9: case 11:
-                if ($dia > 30) {
-                    echo "Fecha incorrecta";
-                } else {
-                    echo "Fecha correcta";
-                }
+                return !($dia > 30);
                 break;
 
             case 2:
                 if (esBisiesto($anio)) {
-                    if ($dia > 29) {
-                        echo "Fecha incorrecta";
-                    } else {
-                        echo "Fecha correcta";
-                    }
-                } else if ($dia > 28) {
-                    echo "Fecha incorrecta";
+                    return !($dia > 29);
                 } else {
-                    echo "Fecha correcta";
+                    return !($dia > 28);
                 }
                 break;
 
